cc2aff3bcb8d26c1364e0917ba635d8c344ca4c1
Sara <khwkekhw@gmail.com>
2025-05-12 23:31:05 +0430

    add get course details by ID API

--- src/services/api/course/courseService.ts
+++ src/services/api/course/courseService.ts
@@ -1,8 +1,19 @@
 import axios from "axios";
 
-export async function getTopCourse(Count:string) {
-    const result=await axios.get(`https://classapi.sepehracademy.ir/api/Home/GetCoursesTop?Count=${Count}`)
+export async function getTopCourse(Count: string) {
+  const result = await axios.get(
+    `https://classapi.sepehracademy.ir/api/Home/GetCoursesTop?Count=${Count}`
+  );
 
-    return result
-    
+  return result;
+}
+
+// get course with id
+
+export async function GetCourseWithId(courseId: string) {
+  const res = await axios.get(
+    `https://classapi.sepehracademy.ir/api/Home/GetCourseDetails?CourseId=${courseId}`
+  );
+
+  return res;
 }
