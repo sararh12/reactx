0f30df3bc9271fe21d12c1f66ea2e34132af6661
Sara <khwkekhw@gmail.com>
2025-05-16 18:48:52 +0430

    change interceptor structure

--- src/services/interceptor/interceptor.ts
+++ src/services/interceptor/interceptor.ts
@@ -1,44 +1,47 @@
-import axios from "axios";
-import { getItem, removeItem } from "../common/storage.service";
-import toast from "react-hot-toast";
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
 
-const baseURL = import.meta.env.VITE_BASE_URL;
-const notifyError = (err: any) => toast.error(err);
-const instance = axios.create({
-  baseURL: baseURL,
+import { toast } from "sonner";
+import { getItem, removeItem } from "../common/storage.service";
+
+interface AxiosErrorMessage {
+  ErrorMessage: string[];
+  status: number;
+}
+
+const baseURL: string = import.meta.env.VITE_BASE_URL;
+
+const instance: AxiosInstance = axios.create({
+  baseURL,
 });
 
-const onSuccess = (response: any) => {
-  return response.data;
+const onSuccess = (response: AxiosResponse) => {
+  return response;
 };
 
-const onError = (err: any) => {
-  console.log(err);
-  if (err.status === 401) {
+const onError = (err: AxiosError<AxiosErrorMessage>) => {
+  if (err.response?.data.ErrorMessage) {
+    err.response.data.ErrorMessage.forEach((errorMessage) => {
+      toast.error(errorMessage);
+    });
+  } else {
+    toast.error("مشکل غیر منتظره ای رخ داد !");
+  }
+
+  if (err.response?.data.status === 401) {
     removeItem("token");
-    toast.error("First Logged in into your account!");
+
+    window.location.pathname = "/auth/login";
   }
 
-  if (
-    err.response.status >= 400 &&
-    err.response.status < 500 &&
-    err.response.status != 401
-  ) {
-    {
-      err.response.data.ErrorMessage[0]
-        ? notifyError(err.response.data.ErrorMessage[0])
-        : notifyError(err.message);
-    }
-  }
-  return Promise.reject(err);
+  Promise.reject(err);
 };
-
-instance.interceptors.response.use(onSuccess, onError);
 
 instance.interceptors.request.use((opt) => {
   const token = getItem("token");
-  if (token) opt.headers.Authorization = "Bearer " + token;
+
+  if (token && token !== null) opt.headers.Authorization = "Bearer " + token;
   return opt;
 });
+instance.interceptors.response.use(onSuccess, onError);
 
 export default instance;
