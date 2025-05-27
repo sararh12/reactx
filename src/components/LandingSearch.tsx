7d54cab8fd801412d9ef11a38cefc7d7318e6243
Sara <khwkekhw@gmail.com>
2025-05-27 23:50:27 +0430

    add close modal and image

--- src/components/LandingSearch.tsx
+++ src/components/LandingSearch.tsx
@@ -2,10 +2,18 @@ import { Search } from "lucide-react";
 import http from "@/services/interceptor/interceptor";
 import { useState } from "react";
 import { Field, Form, Formik } from "formik";
+import pic from "../../public/lovable-uploads/khabar.png";
+import { Link } from "react-router-dom";
 
 const LandingSearch = () => {
   const [isModalShow, setisModalShow] = useState(false);
   const [list, setList] = useState([]);
+
+  const closeModal = () => {
+    setTimeout(() => {
+      setisModalShow(false);
+    }, 700);
+  };
 
   const handleSearch = async (value) => {
     const { data } = await http.get(
@@ -28,7 +36,7 @@ const LandingSearch = () => {
           <Form>
             <Field
               onFocus={() => setisModalShow(true)}
-              onBlur={() => setisModalShow(false)}
+              onBlur={closeModal}
               name="query"
               type="search"
               className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-luko-teal focus:border-luko-teal"
@@ -44,10 +52,21 @@ const LandingSearch = () => {
         </Formik>
       </div>
       {isModalShow && (
-        <div className="p-2 rounded-md absolute mt-3 bg-red-500 w-full">
+        <div className="p-2 rounded-md absolute mt-3 bg-white border border-solid border-gray-400 text-slate-700  w-full">
           {list?.length > 0 ? (
             list.map((item) => {
-              return <div key={item?.courseId}>{item?.title}</div>;
+              return (
+                <Link to={`/courses/${item?.courseId}`}>
+                  <div
+                    key={item?.courseId}
+                    className="flex gap-5 mb-2 items-center"
+                  >
+                    <img src={item?.tumbImageAddress || pic} className="w-16" />
+                    {item?.title}
+                  </div>
+                  ;
+                </Link>
+              );
             })
           ) : (
             <>موردی یافت نشد ...</>
